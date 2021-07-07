ec2971800f9c60319eaae7fa303590b199e546e6
Alex Tugarev <alex@gitpod.io>
2021-07-07 10:38:23 +0000

    Fix prebuilds for projects

--- components/gitpod-protocol/src/gitpod-service.ts
+++ components/gitpod-protocol/src/gitpod-service.ts
@@ -291,6 +291,8 @@ export namespace GitpodServer {
     }
     export interface CreateWorkspaceOptions {
         contextUrl: string;
+        branch?: string;
+        projectId?: string;
         mode?: CreateWorkspaceMode;
     }
     export interface StartWorkspaceOptions {
